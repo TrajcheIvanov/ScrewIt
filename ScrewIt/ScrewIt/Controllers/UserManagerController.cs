bb01f5a99be3a9870cd7260d60102b0e80c3dbf7
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-07-22 20:59:28 +0200

    Implemented Manage,Edit and Delete Users

--- ScrewIt/ScrewIt/Controllers/UserManagerController.cs
+++ ScrewIt/ScrewIt/Controllers/UserManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ScrewIt.Common;
 using ScrewIt.Mappings;
 using ScrewIt.Models;
 using ScrewIt.Repositories;
@@ -29,17 +30,29 @@ namespace ScrewIt.Controllers
             _roleManager = roleManager;
         }
 
-        public IActionResult ManageOverview()
+        public IActionResult ManageOverview(string errorMessage, string successMessage)
         {
+            ViewBag.ErrorMessage = errorMessage;
+            ViewBag.SuccessMessage = successMessage;
+
             var users = _userManager.Users.ToList();
             var usersToViewModel = new List<UserViewModel>();
 
             foreach (var user in users)
             {
                 var role = _db.UserRoles.FirstOrDefault(x => x.UserId == user.Id);
-                var roleName = _db.Roles.FirstOrDefault(x => x.Id == role.RoleId);
+                var roleName = "";
 
-                usersToViewModel.Add(user.ToViewModel(roleName.Name));
+                if (role == null)
+                {
+                    roleName = Helper.Customer;
+                } else
+                {
+                    roleName = _db.Roles.FirstOrDefault(x => x.Id == role.RoleId).Name;
+                }
+
+
+                usersToViewModel.Add(user.ToViewModel(roleName));
             }
 
             return View(usersToViewModel);
@@ -68,6 +81,55 @@ namespace ScrewIt.Controllers
             }
 
             return Ok(usersToViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+
+            var role = _db.UserRoles.FirstOrDefault(x => x.UserId == user.Id);
+
+            var roleName = _db.Roles.FirstOrDefault(x => x.Id == role.RoleId);
+
+            var userToEdit = user.ToViewModel(roleName.Name);
+
+            return View(userToEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserViewModel userForUpdate)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userForUpdate.Id);
+            var role = _db.UserRoles.FirstOrDefault(x => x.UserId == user.Id);
+            var roleName = _db.Roles.FirstOrDefault(x => x.Id == role.RoleId);
+
+            if (userForUpdate.RoleName != roleName.ToString()) 
+            {
+                _db.UserRoles.Remove(role);
+                var roleToAdd = _db.Roles.FirstOrDefault(x => x.Name == userForUpdate.RoleName);
+                await _userManager.AddToRoleAsync(user, roleToAdd.Name);
+            }
+            
+            user.Name = userForUpdate.Name;
+            user.Surname = userForUpdate.Surname;
+            user.Email = userForUpdate.Email;
+            user.PhoneNumber = userForUpdate.PhoneNumber;
+
+            await _userManager.UpdateAsync(user);
+
+
+            return RedirectToAction("ManageOverview", new { SuccessMessage = "User " + user.Name + " edited sucessfully" });
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+
+            var userName = user.UserName;
+            await _userManager.DeleteAsync(user);
+
+            return RedirectToAction("ManageOverview" , new { ErrorMessage = "User " + userName + " deleted sucessfully" });
         }
     }
 }
