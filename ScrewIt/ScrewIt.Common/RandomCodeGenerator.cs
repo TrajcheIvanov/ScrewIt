2b1e473dc5c24619f57f2862e3321e8fac05aee9
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-09-22 18:13:20 +0200

    Made RandomCodeGenerator method

---
+++ ScrewIt/ScrewIt.Common/RandomCodeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrewIt.Common
+{
+    public static class RandomCodeGenerator
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
