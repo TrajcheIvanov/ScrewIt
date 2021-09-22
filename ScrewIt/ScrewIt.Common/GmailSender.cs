6c6c4efeb91e771f2dee1afb20fd23d49e07b7b4
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-09-22 17:57:04 +0200

    Made GmailSender Class

---
+++ ScrewIt/ScrewIt.Common/GmailSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace ScrewIt.Common
+{
+    public class GmailSender
+    {
+        public static void SendGmail(string recipient, string subject, string htmlMessage)
+        {
+            using SmtpClient email = new SmtpClient
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential("maytrymailsender@gmail.com", "maytrymailsender123")
+            };
+
+
+            string from = "maytrymailsender@gmail.com";
+
+            email.Send(from, recipient, subject, htmlMessage);
+        }
+
+    }
+}
+}
